c9ffd46d0686e5848c64657e22b43c13c97d5cce
My name <me@email.com>
2024-03-02 00:48:11 -0600

    created Weather API module

---
+++ Project_3/api_testing/openweathermap_testing.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from pprint import pprint
+import os
+
+API_KEY = os.environ.get('OPENWEATHER_API_KEY')
+url = 'http://api.openweathermap.org/data/2.5/weather'
+
+params = {'q': 'minneapolis', 'appid': API_KEY, 'units': 'imperial'}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+pprint(data)
+
+"""
+example response data
+
+{'base': 'stations',
+ 'clouds': {'all': 20},
+ 'cod': 200,
+ 'coord': {'lat': 44.98, 'lon': -93.2638},
+ 'dt': 1708306701,
+ 'id': 5037649,
+ 'main': {'feels_like': 19.44,
+          'humidity': 72,
+          'pressure': 1015,
+          'temp': 28.18,
+          'temp_max': 30.87,
+          'temp_min': 24.39},
+ 'name': 'Minneapolis',
+ 'sys': {'country': 'US',
+         'id': 2012563,
+         'sunrise': 1708261812,
+         'sunset': 1708299847,
+         'type': 2},
+ 'timezone': -21600,
+ 'visibility': 10000,
+ 'weather': [{'description': 'few clouds',
+              'icon': '02n',
+              'id': 801,
+              'main': 'Clouds'}],
+ 'wind': {'deg': 230, 'speed': 9.22}}
+
+
+"""
--- getWeatherForecast.py
+++ getWeatherForecast.py
@@ -1,41 +1,35 @@
-"""This code provides a simple interface to fetch weather forecast data and store
-it in an instance of the Weather class. The user can then access the weather information
-from the instance variables."""
-
+import os
 import requests
 
 
-class Weather:
-    # method initializes the instance variables to None,
-    # so that the values for these attributes will be set later based on actual weather data
-    def __init__(self) -> None:
-        self.windspeed = None
-        self.icon = None
-        self.conditions = None
-        self.temp = None
+def get_weather_forecast(lon, lat):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
 
-    # method takes three parameters to fetch the weather forecast from the OpenWeatherMap API
-    # uses the provided latitude and longitude to create a query for the API request
-    def get_weather_forecast(self, api_key, lon, lat):
-        base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    api_key = os.getenv("OPENWEATHER_API_KEY")  # Get API key from environment variable
 
-        query_params = {
-            'lat': lat,
-            'lon': lon,
-            'appid': api_key  # You can change this to 'imperial' for Fahrenheit
-        }
-        try:
-            response = requests.get(base_url, params=query_params)
-            # checks if the response contains any HTTP error status codes
-            # and raises an exception if an error is detected
-            response.raise_for_status()
-            # method parses the JSON response to extract relevant weather information
-            # such as wind speed, weather icon, conditions, and temperature
-            forecast_data = response.json()
-            self.windspeed = forecast_data['wind']['speed']
-            self.icon = forecast_data['weather'][0]['icon']
-            self.conditions = forecast_data['weather'][0]['main']
-            self.temp = forecast_data['main']['temp']
-        except Exception as e:
-            # printing detail of exception here
-            print(f"An unexpected error occurred: {e}")
+    if api_key is None:
+        print("API key not found. Please set the OPENWEATHER_API_KEY environment variable.")
+        return None
+
+    query_params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': api_key
+    }
+    try:
+        response = requests.get(base_url, params=query_params)
+        response.raise_for_status()
+        forecast_data = response.json()
+
+        windspeed = forecast_data['wind']['speed']
+        icon = forecast_data['weather'][0]['icon']
+        conditions = forecast_data['weather'][0]['main']
+        temp = forecast_data['main']['temp']
+
+        return windspeed, icon, conditions, temp
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
