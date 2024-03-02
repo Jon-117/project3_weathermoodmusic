6db659507a6568fab45bcb006628a41c9a5389bb
Riley Strandlund <95266158+Riley-Strandlund@users.noreply.github.com>
2024-03-02 02:37:56 -0600

    Update get_location.py
    Deleted JSON structure example for Nominatim.

--- get_location.py
+++ get_location.py
@@ -47,38 +47,3 @@ def format_data(data):
               
     
 main()
-
-
-
-"""
-[{'addresstype': 'city',
-  'boundingbox': ['44.8901500', '45.0512500', '-93.3291271', '-93.1938590'],
-  'class': 'boundary',
-  'display_name': 'Minneapolis, Hennepin County, Minnesota, United States', 
-  'importance': 0.6371693135985456,
-  'lat': '44.9772995',
-  'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. '
-             'http://osm.org/copyright',
-  'lon': '-93.2654692',
-  'name': 'Minneapolis',
-  'osm_id': 136712,
-  'osm_type': 'relation',
-  'place_id': 29019823,
-  'place_rank': 16,
-  'type': 'administrative'},
- {'addresstype': 'town',
-  'boundingbox': ['39.1140825', '39.1322656', '-97.7246031', '-97.6878841'],
-  'class': 'boundary',
-  'display_name': 'Minneapolis, Ottawa County, Kansas, United States',
-  'importance': 0.4283953917728152,
-  'lat': '39.1223968',
-  'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. '
-             'http://osm.org/copyright',
-  'lon': '-97.7087076',
-  'name': 'Minneapolis',
-  'osm_id': 130006,
-  'osm_type': 'relation',
-  'place_id': 316616611,
-  'place_rank': 16,
-  'type': 'administrative'}]
-"""
