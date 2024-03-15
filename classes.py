fdd6eeaf2a9e21cec1f1ba361ebe613223f54e4a
Riley-Strandlund <FFOrstrandlund@gmail.com>
2024-03-15 05:24:25 -0500

    Created database tests and fixed most database query methods

--- classes.py
+++ classes.py
@@ -7,8 +7,13 @@ Weather, Location, WeatherMood, Menu, Playlist
 from dataclasses import dataclass
 import sqlite3
 import os
+from datetime import datetime
 
-db = os.path.join('database', 'playlists.db')
+
+folder = 'database'
+file = 'weatherMood_library.db'
+
+db = os.path.join(folder, file)
 
 class Menu:
     #TODO - Fill in the rest of this class
@@ -27,74 +32,121 @@ class Menu:
 
 @dataclass
 class Weather:
-    def __init__(self, windspeed, icon, conditions, temp):
-        self.windspeed = windspeed
-        self.icon = icon  # Icon related to weather conditions. Many freely available.
-        self.conditions = conditions # Weather conditions
-        self.temp = temp
-        # Potential methods for interacting with weather APIs
+    windspeed: float
+    icon: str  # Icon related to weather conditions. Many freely available.
+    conditions: str  # Weather conditions
+    temp: float
+    # Potential methods for interacting with weather APIs
 
 
 @dataclass 
 class Playlist:
 
-    def __init__(self, song_count, title, url, image_link):
+    def __init__(self, song_count, playlist_title, playlist_url, playlist_image_url):
         self.song_count = song_count
-        self.title = title
-        self.url = url
-        self.image_url = image_link # Link to the playlist image - Useful when flask is implemented
+        self.playlist_title = playlist_title
+        self.playlist_url = playlist_url
+        self.playlist_image_url = playlist_image_url # Link to the playlist image - Useful when flask is implemented
 
     def pretty_string(self):
         return f'{self.title} :: {self.song_count} Songs :: {self.url}'
     # Methods for playlist interaction could be added here
 
+
 class Location:
-    # Placeholder for location attributes and methods
-    def __init__(self, city_name, full_name):
+    def __init__(self, city_name, full_name, latitude, longitude):
         self.city_name = city_name
         self.full_name = full_name
-
-    def weather_query_str(self):
-        # Method to return a string suitable for searching weather API
-        return f'{self.city_name}'  # Better to use a manipulation of full_name?
+        self.latitude = latitude
+        self.longitude = longitude
+    # Create method to display location like Playlist class.
 
 
 class WeatherMood:
+    """
+    WeatherMood class stores the reminiscence
+    """
+    def __init__(self,
+                 id, favorite, created_datetime,
+                 city_name, full_name, latitude, longitude,
+                 temp, windspeed, icon, conditions,
+                 song_count, playlist_title, playlist_image_url, playlist_url):
 
-    def __init__(self, location: Location, weather: Weather, playlist: Playlist, id=None, favorite=False):
-        self.location            = location
-        self.weather             = weather
-        self.playlist            = playlist
+        # WeatherMood Specific
+        self.id = id
+        self.favorite = favorite
+        self.created_datetime = created_datetime if created_datetime else datetime.now()   # Date created. Doesn't change if it's supplied (ie, when recreating object from db)
+        # Location derived
+        self.city_name = city_name
+        self.full_name = full_name
+        self.latitude = latitude
+        self.longitude = longitude
+        # Weather derived
+        self.temp = temp
+        self.windspeed = windspeed
+        self.icon = icon
+        self.conditions = conditions
+        # Playlist derived
+        self.song_count = song_count
+        self.playlist_title = playlist_title
+        self.playlist_image_url = playlist_image_url
+        self.playlist_url = playlist_url
 
-        self.city_name           = self.location.city_name
-        self.full_name           = self.location.full_name
+        self.weathermoodlibrary = WeatherMoodLibrary()
 
-        self.temp                = self.weather.temp
-        self.windspeed           = self.weather.windspeed
-        self.icon                = self.weather.icon
-        self.conditions          = self.weather.conditions
-
-        self.song_count          = self.playlist.song_count
-        self.playlist_title      = self.playlist.title
-        self.playlist_image_url  = self.playlist.image_url
-        self.playlist_url        = self.playlist.url
-
-        self.id                  = id
-        self.favorite            = favorite
-
-        self.playlistLibrary     = PlaylistLibrary()
-
-    def save(self):
-        self.playlistLibrary.add_playlist(self)
+    def display_string(self) -> str:
+        display_string = f"{self.conditions.title()} in {self.city_name}... {self.playlist_title}: {self.playlist_url}"
+        return display_string
     
-    def delete(self):
-        self.playlistLibrary.delete_all_playlists(self)
-    
-        
-        
+    def delete(self): 
+        """ Can't delete without the 'self' from this class """
+        self.weathermoodlibrary.delete_playlist(self)
 
 
-class PlaylistLibrary:
+class WeatherMoodBuilder:
+    def build(self, location, weather, playlist):
+        # Extracting information from the Location object
+        city_name = location.city_name
+        full_name = location.full_name
+        latitude = location.latitude
+        longitude = location.longitude
+
+        # Extracting information from the Weather object
+        temp = weather.temp
+        windspeed = weather.windspeed
+        icon = weather.icon
+        conditions = weather.conditions
+
+        # Extracting information from the Playlist object
+        song_count = playlist.song_count
+        playlist_title = playlist.playlist_title
+        playlist_image_url = playlist.playlist_image_url
+        playlist_url = playlist.playlist_url
+
+        # Building and returning the WeatherMood object
+        return WeatherMood(
+            id                 = None, # Set after storage in db
+            favorite           = None, # Set after storage in db
+            created_datetime   = None,
+
+            city_name          = city_name,
+            full_name          = full_name,
+            latitude           = latitude,
+            longitude          = longitude,
+
+            temp               = temp,
+            windspeed          = windspeed,
+            icon               = icon,
+            conditions         = conditions,
+            
+            song_count         = song_count,
+            playlist_title     = playlist_title,
+            playlist_image_url = playlist_image_url,
+            playlist_url       = playlist_url
+        )
+
+
+class WeatherMoodLibrary:
     """ What functions are necessary?
             - Display favorites
             - Display All
@@ -103,10 +155,10 @@ class PlaylistLibrary:
         """
     instance = None
 
-    class __PlaylistLibrary:
+    class __WeatherMoodLibrary:
 
         def __init__(self):
-            create_table_sql = 'CREATE TABLE IF NOT EXISTS playlists (city_name TEXT, full_name TEXT, temp FLOAT, windspeed FLOAT, icon TEXT, conditions TEXT, song_count TEXT, playlist_title TEXT, playlist_image_url TEXT, playlist_url TEXT, favorite BOOLEAN)'
+            create_table_sql = 'CREATE TABLE IF NOT EXISTS weatherMood_library (city_name TEXT, full_name TEXT, latitude FLOAT, longitude FLOAT, temp FLOAT, windspeed FLOAT, icon TEXT, conditions TEXT, song_count TEXT, playlist_title TEXT, playlist_image_url TEXT, playlist_url TEXT, created_datetime DATETIME, favorite TINYINT, UNIQUE( full_name COLLATE NOCASE, conditions COLLATE NOCASE ))'
             # Icon will be set as text assuming we save a url or filepath
 
             con = sqlite3.connect(db)
@@ -118,98 +170,117 @@ class PlaylistLibrary:
         
         # We need to create a database entry when a link is accessed
         def add_playlist(self, weatherMood):
-            insert_sql = 'INSERT INTO playlists (city_name, full_name, temp, windspeed, icon, conditions, song_count, playlist_title, playlist_image_url, playlist_url, favorite) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+            insert_sql = 'INSERT INTO weatherMood_library (city_name, full_name, latitude, longitude, temp, windspeed, icon, conditions, song_count, playlist_title, playlist_image_url, playlist_url, created_datetime, favorite) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
             try:
                 with sqlite3.connect(db) as con:
-                    res = con.execute(insert_sql, (weatherMood.city_name, weatherMood.full_name, weatherMood.temp, weatherMood.windspeed, weatherMood.icon, weatherMood.conditions, weatherMood.song_count, weatherMood.playlist_title, weatherMood.playlist_image_url, weatherMood.playlist_url, weatherMood.favorite))
+                    res = con.execute(insert_sql, (weatherMood.city_name, weatherMood.full_name, weatherMood.latitude, weatherMood.longitude, weatherMood.temp, weatherMood.windspeed, weatherMood.icon, weatherMood.conditions, weatherMood.song_count, weatherMood.playlist_title, weatherMood.playlist_image_url, weatherMood.playlist_url, weatherMood.created_datetime, weatherMood.favorite))
                     new_id = res.lastrowid # Get the ID of the new row in the table
                     weatherMood.id = new_id
+                    weatherMood.favorite = 0
 
                 # The below code should delete rows if it exceeds a certain amount
                 cursor = con.cursor()
-                cursor.execute('SELECT COUNT(*) FROM playlists') 
+                cursor.execute('SELECT COUNT(*) FROM weatherMood_library') 
                 total_row = cursor.fetchone()[0]
 
                 if total_row > 10: 
                     excess_row = total_row - 10 # Finds out how many rows are excess
-                    delete_excess_rows = 'DELETE FROM playlists WHERE id IN (SELECT id FROM pllaylists ORDER BY id LIMIT ?)'
+                    delete_rows = excess_row
+                    delete_excess_rows = 'DELETE FROM weatherMood_library WHERE rowid IN (SELECT rowid FROM weatherMood_library LIMIT ?)'
                     with sqlite3.connect(db) as con:
-                        con.execute(delete_excess_rows, (excess_row))
+                        con.execute(delete_excess_rows, (delete_rows,))
 
             except sqlite3.IntegrityError as e:
-                raise PlaylistErrors(f'Error - this weather mood already exists. {weatherMood}') from e
+                raise WeatherMoodErrors(f'Error - this weather mood already exists. {weatherMood}') from e
             finally:
                 con.close()
         
-
         def delete_playlist(self, weatherMood):
-            if not weatherMood.id:
-                raise PlaylistErrors(f'id does not exist')
 
-            delete_sql = 'DELETE FROM playlists WHERE rowid = ?'
+            if not weatherMood.id:
+                raise WeatherMoodErrors(f'id does not exist')
+
+            delete_sql = 'DELETE FROM weatherMood_library WHERE rowid = ?'
 
             with sqlite3.connect(db) as con:
-                deleted = con.execute(delete_sql, (id, ))
+                deleted = con.execute(delete_sql, (weatherMood.id, ))
                 deleted_count = deleted.rowcount
-            con.close()
 
             if deleted_count == 0:
-                raise PlaylistErrors(f'Could not find the id. Deletion could not occur.')
+                raise WeatherMoodErrors(f'Could not find the id. Deletion could not occur.')
 
         def delete_all_playlists(self):
-            delete_all_sql = 'DELETE FROM playlists'
+            delete_all_sql = 'DELETE FROM weatherMood_library'
 
             with sqlite3.connect(db) as con:
                 deleted = con.execute(delete_all_sql)
             con.close()
 
         def list_all_playlists(self):
-            """ Fetches a limited amount of playlist rows."""
-            get_playlists_sql = 'SELECT rowid, * FROM playlists LIMIT 10'
+            """ Fetches a limited amount of weather mood rows."""
+            get_playlists_sql = 'SELECT rowid, * FROM weatherMood_library LIMIT 10'
             con = sqlite3.connect(db)
             con.row_factory = sqlite3.Row
             rows = con.execute(get_playlists_sql)
             playlists = []
 
             for r in rows:
-                playlist = PlaylistLibrary(r['playlist_title'], r['full_name'], r['condition'], r['playlist_url'])
+                playlist = WeatherMood(r['rowid'],
+                    r['favorite'], r['created_datetime'],
+                    r['city_name'],r['full_name'],
+                    r['latitude'], r['longitude'],
+                    r['temp'], r['windspeed'], 
+                    r['icon'],r['conditions'],
+                    r['song_count'], r['playlist_title'],
+                    r['playlist_image_url'], r['playlist_url'] )
                 playlists.append(playlist)
 
             con.close()
 
             return playlists
         
-        def display_favorites(self, read):
-            select_favorites_sql = 'SELECT favorite FROM playlist WHERE favorite = ?'
+        def display_favorites(self, favorite):
+            select_favorites_sql = 'SELECT favorite FROM weatherMood_library WHERE favorite = ?'
             con = sqlite3.connect(db)
             con.row_factory = sqlite3.Row
-            fav_rows = con.execute(select_favorites_sql, (read, ))
+            fav_rows = con.execute(select_favorites_sql, (favorite, ))
             fav_playlists = []
 
             for r in fav_rows:
-                playlist = PlaylistLibrary(r['playlist_title'], r['full_name'], r['condition'], r['playlist_url'])
+                playlist = WeatherMoodLibrary(r['playlist_title'], r['full_name'], r['condition'], r['playlist_url'])
                 fav_playlists.append(playlist)
             con.close()
 
             return fav_playlists
 
         def get_playlist_by_id(self, id):
-            get_playlist_id_sql = 'SELECT rowid, * FROM playlists WHERE rowid = ?'
+
+            get_playlist_id_sql = 'SELECT rowid, * FROM weatherMood_library WHERE rowid = ?'
 
             con = sqlite3.connect(db)
             con.row_factory = sqlite3.Row
             rows = con.execute(get_playlist_id_sql, (id, ))
             playlist_data = rows.fetchone()
+            playlist = None
 
             if playlist_data:
-                playlist = WeatherMood(playlist_data['location: Location'], playlist_data['weather: Weather'], playlist_data['playlist: Playlist'], playlist_data['id'], playlist_data['favorite'])
-            con.close()    
+                playlist = WeatherMood(
+                    playlist_data['rowid'],
+                    playlist_data['favorite'], playlist_data['created_datetime'],
+                    playlist_data['city_name'],playlist_data['full_name'],
+                    playlist_data['latitude'], playlist_data['longitude'],
+                    playlist_data['temp'], playlist_data['windspeed'], 
+                    playlist_data['icon'],playlist_data['conditions'],
+                    playlist_data['song_count'], playlist_data['playlist_title'],
+                    playlist_data['playlist_image_url'], playlist_data['playlist_url']  
+                 )
+            con.close()
             return playlist
 
     def __new__(cls):
-        if not PlaylistLibrary.instance:
-            PlaylistLibrary.instance = PlaylistLibrary.__PlaylistLibrary()
-        return PlaylistLibrary.instance
+        if not WeatherMoodLibrary.instance:
+            WeatherMoodLibrary.instance = WeatherMoodLibrary.__WeatherMoodLibrary()
+        return WeatherMoodLibrary.instance
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
@@ -217,6 +288,25 @@ class PlaylistLibrary:
     def __setattr__(self, name, value):
         return setattr(self.instance, name, value)
 
-class PlaylistErrors(Exception):
+class WeatherMoodErrors(Exception):
     """ For errors """
     pass
+
+
+# def main():
+    
+
+# main()
+#Reference for adding data to the database. Also found in 'database_testing'.
+# def main():
+#     place = Location('austin', 'austin, tx', '23.22','-32.24')
+#     playlist = Playlist('3', 'Country', 'https://UrlOfImage.com','https://WeatherMood.com')
+#     weather = Weather('9.9', '12.2', 'https://NotImage.com', 'Thunderstorm')
+
+#     builder = WeatherMoodBuilder()
+#     compiled_data= builder.build(place, weather, playlist)
+#     library = WeatherMoodLibrary()
+#     library.add_playlist(compiled_data)
+   
+# main()
+
---
+++ tests/database_testing.py
@@ -0,0 +1,87 @@
+""" 
+This file tests the different methods for querying the database.
+Make sure to change your file path to run this. 
+"""
+import os
+import unittest
+from unittest import TestCase
+
+import sys
+sys.path.append("..") # Necessary to  find 'classes' file
+import classes
+from classes import Weather, Playlist, Location, WeatherMoodBuilder, WeatherMoodLibrary, WeatherMoodErrors
+
+
+
+class TestWeatherMood_Library(TestCase):
+    
+    @classmethod
+    def prepDatabase(cls):
+        classes.db = os.path.join('database', 'test_playlists.db')
+        WeatherMoodLibrary.instance = None
+    
+    def clear_database(self):
+        library = WeatherMoodLibrary()
+        library.delete_all_playlists()
+
+    def add_weatherMood(self):
+        builder = WeatherMoodBuilder()
+        library = WeatherMoodLibrary()
+        
+        location = Location('Plymouth', 'Plymouth, MN', '-23.33','32.24')
+        weather = Weather('40.00', '10.00', 'Https://icon.com', 'Rainy/Cloudy')
+        playlist = Playlist('14', 'Dubstep Boom', 'https://imageurl.com', 'https://playlist.com')
+
+        object = builder.build(location, weather, playlist) # builder extracts data from api classes and gives it to the WeatherMood class
+        
+        library.add_playlist(object)
+
+    def test_display_all(self):
+        library = WeatherMoodLibrary()
+        self.add_weatherMood()
+        all_weatherMoods = library.list_all_playlists()
+
+        for item in all_weatherMoods:
+            print(item)
+        self.clear_database()
+
+    def test_delete_playlist(self):
+        # If unittest is ran without 'test_delete_all' it errors after two runs, because it can't properly grab a row.
+        self.add_weatherMood()
+        library = WeatherMoodLibrary()
+        playlist_id = 1
+        returned_data = library.get_playlist_by_id(playlist_id)
+        returned_data.delete()
+
+    def test_duplicate_error(self):
+        self.add_weatherMood()
+        with self.assertRaises(WeatherMoodErrors):
+            self.add_weatherMood()
+
+        self.clear_database()
+
+    def test_delete_all(self):
+        builder = WeatherMoodBuilder()
+        library = WeatherMoodLibrary()
+
+        location = Location('Minneapolis', 'Minneapolis, MN', '-22.33','12.24')
+        weather = Weather('45.03', '12.02', 'Getimages.com', 'Rainbow')
+        playlist = Playlist('9', 'Kinetic Disco', 'https://Sometimes.com', 'https://WeatherMood.com')
+
+        location2 = Location('Austin', 'Austin, TX', '10.33','-23.24')
+        weather2 = Weather('16.00', '15.39', 'https://something.com', 'Sunny')
+        playlist2 = Playlist('12', 'Rock n Pop', 'https://somethingWeird.com', 'https://weatherMood.com')
+
+        object = builder.build(location, weather, playlist)
+        object2 = builder.build(location2, weather2, playlist2) 
+
+        library.add_playlist(object)
+        library.add_playlist(object2)
+
+        self.clear_database()
+        
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
