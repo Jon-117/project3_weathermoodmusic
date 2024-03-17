c6e921001eed70bf4625bd95dec97ae12113cca6
Jon-117 <38222965+Jon-117@users.noreply.github.com>
2024-03-17 14:43:37 -0500

    pulled in some changes from @hm2247fg's UI-MAIN module for get_location.py.
    Also made slight changes to docstrings.

--- get_location.py
+++ get_location.py
@@ -1,11 +1,14 @@
 """
-Grab location from API based on users input. Currently set up to search for city names based on input only in the United States.
+Grab location from API based on users input.
 
-Currently when looking up by state only returns the state instead of all cities in the state
+Currently set up to search for city names.
 """
 
 import requests
 from classes import Location
+import logging
+
+log = logging.getLogger(__name__)
 
 params = {
     'city': '',
@@ -16,11 +19,19 @@ params = {
 
 def get_location(user_input):
     """Gather all methods into one in order to create location object. 
-    Only needs to call this function to excecute all of them."""
+    Only needs to call this function to execute all of them."""
     try:
         create_params(user_input)
         returned_data = request_nominatim()
-        return format_data(returned_data)
+        result = format_data(returned_data)
+        i = 0
+        if len(result) > 1:
+            for item in result:
+                print(i, item.city_name, item.full_name)
+                i += 1
+            choice = int(input(f'Enter choice 0-{i-1}: '))
+            return result[choice]
+        return result[0]
 
     except requests.exceptions.HTTPError as e:
         print("An HTTP error has occurred.", e)
@@ -32,6 +43,7 @@ def create_params(user_input):
     """ Creates the search string for the api to search"""
     if user_input.isdigit():  # Prevents number input
         raise ValueError("Input must be a city name.")
+
     user_input = user_input
     search_string = f'{user_input}'
     params.update({'city': search_string})
@@ -47,16 +59,19 @@ def request_nominatim():
 def format_data(data):
     """ Formats the data by taking 'display_name' and splitting it into [city, county, state, country]
     while filtering out unnecessary info. The location names will be used for display purposes and long and lat will be used for openweather"""
+    location_objects = []
     for info in data:
         location = info['display_name'].split(',')  # location is used in the UI to display the location to the user.
         if len(location) > 4:  # Zip Code can appear between State and Country which this filters out.
             location.remove(location[3])
-        location.remove(location[3])
-        location.remove(location[1])
+        # location.remove(location[3])
+        # location.remove(location[1])
         city = location[0]
         state = location[1]
         # Latitude and longitude can be used for openweather to locate the area.
         latitude = info['lat']
         longitude = info['lon']
         location_object = Location(city, state, latitude, longitude)
-        return location_object
+        location_objects.append(location_object)
+    return location_objects
+
