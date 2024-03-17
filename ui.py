5e58d17ea7964bf83f50729ee36fdf16bbb7126a
Jon-117 <38222965+Jon-117@users.noreply.github.com>
2024-03-17 16:55:15 -0500

    Further validation and logging added to get_selection().

--- ui.py
+++ ui.py
@@ -69,22 +69,32 @@ def get_user_input(prompt: str, is_integer=False, is_float=False):
             alert("Error getting user input, please try again.")
 
 
-def get_selection(menu_options):
+def get_selection(menu_options: list or dict):
+    """
+    Get a user's selection from supplied list or dictionary
+    """
+    log.info('Creating selection options... ')
     indexed_options = {idx + 1: option for idx, option in enumerate(menu_options)}
     for idx, option in indexed_options.items():
-        print(f'[{idx}] {option}')
+        log.info(f'Presenting Option: {idx}: {option} ')
+        show_message(f'[{idx}] {option}')
 
     while True:
-        user_input = get_user_input('Choose an option: ')
+        log.info('Getting user input for selection... ')
+        user_input = get_user_input('Choose an option:  ', is_integer=True)
+        log.info(f'User input for selection: {str(user_input)}')
         try:
-            selection = int(user_input)
-            if selection in indexed_options:
-                show_message(indexed_options[selection])
-                return indexed_options[selection]
+            log.debug('Validating user input for selection... ')
+            if user_input in indexed_options:
+                log.debug(f'Selection validated. \nSelected "{user_input}: {indexed_options[user_input]}"')
+                show_message(indexed_options[user_input])
+                log.debug(f'Returning {indexed_options[user_input]}')
+                return indexed_options[user_input]
             else:
+                log.debug(f'Input "{user_input}" is not valid. ')
                 alert('Please select a valid option.')
         except ValueError:
-            alert('Please enter a number.')
+            alert('Please enter a valid number.')
 
 
 def confirm_choice() -> bool:
